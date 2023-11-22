f97ce13286c5a56e9df36fd3580002454e5ba624
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-11-22 15:58:42 +0200

    💄restourant page - add images  and reviews parts

--- app/page.tsx
+++ app/page.tsx
@@ -76,6 +76,79 @@ export default function Home() {
               </p>
             </div>
             {/* DESCRIPTION */}
+
+            {/* IMAGES */}
+            <div>
+              <h1 className="mt-10 mb-7 border-b pb-5 text-3xl font-bold">
+                7 photos
+              </h1>
+              <div className="flex flex-wrap">
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/3/56612519.webp"
+                  alt=""
+                  className="mr-1 mb-1 h-44 w-56"
+                />
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/3/47003715.webp"
+                  alt=""
+                  className="mr-1 mb-1 h-44 w-56"
+                />
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/2/47003711.webp"
+                  alt=""
+                  className="mr-1 mb-1 h-44 w-56"
+                />
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/2/47003713.webp"
+                  alt=""
+                  className="mr-1 mb-1 h-44 w-56"
+                />
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/3/56612519.webp"
+                  alt=""
+                  className="mr-1 mb-1 h-44 w-56"
+                />
+              </div>
+            </div>
+            {/* IMAGES */}
+
+            {/* REVIEWS */}
+            <div>
+              <h1 className="mt-10 mb-7 border-b pb-5 text-3xl font-bold">
+                What 100 people are saying
+              </h1>
+              <div>
+                {/* REVIEW CARD */}
+                <div className="m mb-7 border-b pb-7">
+                  <div className="flex">
+                    <div className="flex w-1/6 flex-col items-center">
+                      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-blue-400">
+                        <h2 className="text-2xl text-white">MJ</h2>
+                      </div>
+                      <p className="mt-2 text-center">Micheal Jordan</p>
+                    </div>
+                    <div className="ml-10 w-5/6">
+                      <div className="flex items-center">
+                        <div className="mr-5 flex">*****</div>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-lg font-light">
+                          Had a lovely time celebrating my two friends 30th
+                          birthdays. I arrived early to give the staff a cake
+                          and candles and paid for a bottle of Prosecco. When we
+                          arrived at the table, the Prosecco was there waiting.
+                          The staff were great, food was delicious and our two
+                          birthdays girls got a complimentary table after the
+                          cake was brought out! Fabulous evening!
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* REVIEW CARD */}
+              </div>
+            </div>
+            {/* REVIEWS */}
           </div>
         </div>
         {/* DESCRIPTION PORTION */}
