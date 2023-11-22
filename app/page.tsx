79f8b80060a000c996a8220ecf6d8a6433f88631
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-11-22 12:33:14 +0200

    💄add card

--- app/page.tsx
+++ app/page.tsx
@@ -48,6 +48,37 @@ export default function Home() {
             </div>
           </div>
           {/* HEADER */}
+
+          {/* CARDS */}
+          <div className="mt-10 flex flex-wrap py-3 px-36">
+            {/* CARD */}
+            <div className="m-3 h-72 w-64 cursor-pointer overflow-hidden rounded border">
+              <img
+                src="https://resizer.otstatic.com/v2/photos/wide-huge/3/56612519.webp"
+                alt=""
+                className="h-36 w-full"
+              />
+              <div className="p-1">
+                <h3 className="mb-2 text-2xl font-bold">
+                  Mr White's Leicester
+                </h3>
+                <div className="flex items-start">
+                  <div className="mb-2 flex">****</div>
+                  <p className="ml-2">77 reviews</p>
+                </div>
+                <div className="flex text-reg font-light capitalize">
+                  <p className="mr-3">mexican</p>
+                  <p className="mr-3">$$$$</p>
+                  <p>Mazeikai</p>
+                </div>
+
+                <p className="mt-2 text-sm font-bold">Booked 3 times today</p>
+              </div>
+            </div>
+            {/* CARD */}
+          </div>
+
+          {/* CARDS */}
         </main>
       </main>
     </main>
