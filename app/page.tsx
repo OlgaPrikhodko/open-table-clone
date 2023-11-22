d5e803b49f88551debd41d9c7db1e83200505815
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-11-22 12:09:49 +0200

    💄add header with searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,31 @@ export default function Home() {
           </div>
         </nav>
         {/* NAVBAR */}
+
+        <main>
+          {/* HEADER */}
+          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+            <div className="mt-10 text-center">
+              <h1 className="text-5xl font-bold text-white">
+                Find your table for any occasion
+              </h1>
+
+              {/* SEARCHBAR */}
+              <div className="m-auto flex justify-center py-7 text-left text-lg">
+                <input
+                  className="mr-3 w-[450px] rounded p-2"
+                  type="text"
+                  placeholder="State, city or town"
+                />
+                <button className="rounded bg-red-600 px-9 py-2 text-white">
+                  Let's go!
+                </button>
+              </div>
+              {/* SEARCHBAR */}
+            </div>
+          </div>
+          {/* HEADER */}
+        </main>
       </main>
     </main>
   );
