c80f84995aaea9eda9ed945c979322c7945b16b9
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-11-22 11:51:38 +0200

    💄add nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className="text-blue-400 text-7xl">Hello</h1>
+    <main className="min-h-screen w-screen bg-gray-100">
+      <main className="m-auto max-w-screen-2xl bg-white">
+        {/* NAVBAR */}
+        <nav className="flex justify-between bg-white p-2">
+          <a href="" className="text-2xl font-bold text-gray-700">
+            OpenTable
+          </a>
+
+          <div>
+            <div className="flex">
+              <button className="mr-3 rounded border bg-blue-400 p-2 px-4 text-white">
+                SignIn
+              </button>
+              <button className="rounded border p-2 px-4">SignUp</button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+      </main>
     </main>
   );
 }
