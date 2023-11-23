62aea55e175459a338ebeaf9c7415cdb225bd2e1
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-11-23 22:24:59 +0200

    :star: render Title in the header in the Restaurant Page

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,9 +1,15 @@
-function Header() {
+function Header({ slug }: { slug: string }) {
+  const renderTitle = () => {
+    const nameArray = slug.split("-");
+    nameArray[nameArray.length - 1] = `(${nameArray.at(-1)})`;
+    return nameArray.join(" ");
+  };
+
   return (
     <div className="h-96 overflow-hidden">
       <div className="flex h-full items-center justify-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] bg-center">
         <h1 className="text-center text-7xl capitalize text-white drop-shadow">
-          Mr White's Leicester (Mazeikiai)
+          {renderTitle()}
         </h1>
       </div>
     </div>
--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
 
-function RestaurantLayout({ children }: { children: React.ReactNode }) {
+function RestaurantLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
+}) {
   return (
     <main>
-      <Header />
+      <Header slug={params.slug} />
 
       <div className="0 m-auto -mt-11 flex w-2/3 items-start justify-between">
         {children}
