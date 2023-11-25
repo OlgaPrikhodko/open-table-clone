cdaf66fb6605b64d46aaa067209efccd70af2ed3
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-11-25 13:59:32 +0200

    :star:  apply review data for the restaurant detais page

--- app/restaurant/[slug]/components/Rating.tsx
+++ app/restaurant/[slug]/components/Rating.tsx
@@ -1,12 +1,15 @@
-function Rating() {
+import { Review } from "@prisma/client";
+import { calculateReviewRatingAverage } from "../../../utils/calculateReviewRatingAverage";
+
+function Rating({ reviews }: { reviews: Review[] }) {
   return (
     <div className="flex items-end pt-2">
       <div className="ratings flex items-center">
         <p>*****</p>
-        <p className="ml-3 text-reg">4.9</p>
+        <p className="ml-3 text-reg">{calculateReviewRatingAverage(reviews)}</p>
       </div>
       <div>
-        <p className="ml-4 text-reg">600 reviews</p>
+        <p className="ml-4 text-reg">{reviews.length} reviews</p>
       </div>
     </div>
   );
--- app/restaurant/[slug]/components/Reviews.tsx
+++ app/restaurant/[slug]/components/Reviews.tsx
@@ -1,37 +1,18 @@
-function Reviews() {
+import { Review } from "@prisma/client";
+import ReviewCard from "./ReviewCard";
+
+function Reviews({ reviews }: { reviews: Review[] }) {
   return (
     <div>
       <h1 className="mt-10 mb-7 border-b pb-5 text-3xl font-bold">
-        What 100 people are saying
+        What {reviews.length} {reviews.length > 1 ? "people are " : "person is"}{" "}
+        saying
       </h1>
+
       <div>
-        {/* REVIEW CARD */}
-        <div className="m mb-7 border-b pb-7">
-          <div className="flex">
-            <div className="flex w-1/6 flex-col items-center">
-              <div className="flex h-16 w-16 items-center justify-center rounded-full bg-blue-400">
-                <h2 className="text-2xl text-white">MJ</h2>
-              </div>
-              <p className="mt-2 text-center">Micheal Jordan</p>
-            </div>
-            <div className="ml-10 w-5/6">
-              <div className="flex items-center">
-                <div className="mr-5 flex">*****</div>
-              </div>
-              <div className="mt-5">
-                <p className="text-lg font-light">
-                  Had a lovely time celebrating my two friends 30th birthdays. I
-                  arrived early to give the staff a cake and candles and paid
-                  for a bottle of Prosecco. When we arrived at the table, the
-                  Prosecco was there waiting. The staff were great, food was
-                  delicious and our two birthdays girls got a complimentary
-                  table after the cake was brought out! Fabulous evening!
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* REVIEW CARD */}
+        {reviews.map((review) => (
+          <ReviewCard review={review} key={review.id} />
+        ))}
       </div>
     </div>
   );
