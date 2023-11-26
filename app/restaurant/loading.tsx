21ca819e63d7f7e30ebe87b912c929f0a57bb3f4
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-11-26 11:03:04 +0200

    :bug: fix restaurant loading page

--- app/restaurant/loading.tsx
+++ app/restaurant/loading.tsx
@@ -2,9 +2,9 @@ export default function Loading() {
   return (
     <main>
       <div className="h-96 animate-pulse overflow-hidden bg-slate-200">
-        <div className={`h-full bg-center`} />
+        <div className="h-full bg-center" />
       </div>
-      <div className="0  m-auto -mt-9 flex w-2/3 items-start justify-between">
+      <div className="m-auto -mt-9 flex w-2/3 items-start justify-between">
         <div className="w-[70%] rounded bg-white p-3 shadow">
           <nav className="flex border-b pb-2 text-reg">
             <h4 className="mr-7">Overview</h4>
