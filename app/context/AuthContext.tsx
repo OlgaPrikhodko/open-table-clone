144ba98221356ed8710fb8608d496e2a238dd581
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-12-03 11:55:20 +0200

    :star: Auth: fetch user when opening the app

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { createContext, ReactNode, useState } from "react";
+import axios from "axios";
+import { getCookie } from "cookies-next";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 type User = {
   id: number;
@@ -34,6 +36,38 @@ export default function AuthContext({ children }: { children: ReactNode }) {
     data: null,
     error: null,
   });
+
+  const fetchUser = async () => {
+    setAuthState({ data: null, loading: true, error: null });
+
+    try {
+      const jwt = getCookie("jwt");
+
+      if (!jwt) {
+        return setAuthState({ data: null, loading: false, error: null });
+      }
+
+      const response = await axios.get("http://localhost:3000/api/auth/me", {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+
+      setAuthState({ data: response.data, loading: false, error: null });
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        loading: false,
+        error: error.response.data.errorMessage,
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
     <AuthenticationContext.Provider value={{ ...authState, setAuthState }}>
