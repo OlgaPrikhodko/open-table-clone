526b69b5b68a29cf85ae58661bd832b825ce2905
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-12-09 18:51:37 +0200

    :star: Availability: fetch bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { times } from "../../../../data";
+
+const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
@@ -22,7 +25,21 @@ export default async function handler(
     return res.status(400).json({ errorMessage: "Invalid time data provided" });
   }
 
-  return res.json({ searchTimes });
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes.at(-1)}`),
+      },
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  return res.json({ searchTimes, bookings });
 }
 
-// http://localhost:3000/api/restaurant/sofia-toronto/availability?day=2023-01-01&time=20:00:00.000Z&partySize=4
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-05-01&time=20:00:00.000Z&partySize=4
